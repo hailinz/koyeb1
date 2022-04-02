8af70f49b1a2ef4ea9bf23965fb14817e76f8a42
Hailin <plzyp163@163.com>
2022-04-02 22:58:36 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "0eab213c-92e1-43db-9b86-6cc0b21d2651",
                         "alterId": 0
                     }
                 ],
